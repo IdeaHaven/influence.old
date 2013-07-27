2d34e87c3387d68fe92fb6e0b9eb2f6cd8c437f2
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-07-27 12:13:47 -0700

    issue #21 WIP - need to write controller tests and service API tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,6 +4,7 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular.js',
+  'app/lib/bootstrap.js',  // this was written with angular modules and is a dep for some tests
   'test/lib/angular/angular-mocks.js',
   'app/js/*.js',
   'test/unit/**/*.js'
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,15 +2,29 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('controllers', function() {
+  beforeEach(module('influences.controllers', ['ui.bootstrap', 'influences.services']));
 
+  describe('IndividualCtrl', function() {
+    var scope, ctrl, service;
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    beforeEach(inject(function($rootScope, $controller, Api_sunlight_get) {
+      scope = $rootScope.$new();
+      ctrl = $controller('IndividualCtrl', {$scope: scope, Api_sunlight_get: Api_sunlight_get});
+    }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    it('should pull reps data from the API by zip', function() {
+      inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        ctrl = $controller("IndividualCtrl", {
+          $scope: scope
+        });
+      });
+      console.log('2Scope: ', scope);
+      scope.zip = 44313;
+      scope.get_rep_data_by_zip();
+      expect(scope.reps.length).toEqual(5);
+    });
+
+  });
 });
